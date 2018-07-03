5b996e49050b41388e0698884f7c9f4090ba4725
Yoonku Hwang <yz0624@gmail.com>
2018-07-03 09:55:41 +0900

    add conv2d

--- main.cpp
+++ main.cpp
@@ -8,11 +8,37 @@ void add(double* in1, double* in2, double* out, int n)
 		out[i] = in1[i] + in2[i];
 }
 
+void conv_2d(int kCols, int kRows, double* kernel, int rows, int cols, double* in, double* out)
+{
+	int kCenterX = kCols / 2;
+	int kCenterY = kRows / 2;
+
+	for(int i=0; i < rows; ++i)
+	{
+	    for(int j=0; j < cols; ++j)
+	    {
+	        for(int m=0; m < kRows; ++m)
+	        {
+	            int mm = kRows - 1 - m;
+
+	            for(int n=0; n < kCols; ++n)
+	            {
+	                int nn = kCols - 1 - n;
+
+	                int ii = i + (kCenterY - mm);
+	                int jj = j + (kCenterX - nn);
+
+	                if( ii >= 0 && ii < rows && jj >= 0 && jj < cols )
+	                    out[ i * cols + j] += in[ ii * cols + jj] * kernel[mm * kCols + nn];
+	            }
+	        }
+	    }
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	int N = 5;
-	if(argc > 1)
-		N = atoi(argv[1]);
 
 	double* a = new double[N];
 	double* b = new double[N];
